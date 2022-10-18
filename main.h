62a8fc60e52b97a1f6bf0562c2acf6d17019951b
Kholofelo Tladi <tladi.kholofelo@gmail.com>
2022-10-18 16:35:41 -0700

    Update main.h

--- main.h
+++ main.h
@@ -75,8 +75,6 @@ int (*get_func(char c))(va_list, flag_t *, mod_t *);
 
 /* Sets flags */
 int get_flags(char elem, flag_t *);
-int get_flag(char *s, params_t *params);
-char *get_width(char *s, params_t *params, va_list ap);
 int get_width(const char *format, int *i, va_list list);
 
 /* Sets the modifiers */
