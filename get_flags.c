515832e7c370f1ed9dc8a6ce384ed63c8f1a9c93
Lucan <111079027+lucankinnear@users.noreply.github.com>
2022-10-19 02:56:04 +0200

    Update get_flags.c

--- get_flags.c
+++ get_flags.c
@@ -2,9 +2,9 @@
 
 /**
  * get_flags - Calculates active flags
- * @format: Formatted string in which to print the arguments
- * @i: take a parameter.
- * Return: Flags:
+ * @format: Formatted string to print the arguments
+ * @i: takes a parameter.
+ * Return: flags
  */
 int get_flags(const char *format, int *i)
 {
